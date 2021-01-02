9b0c0c8ea91dceeb5bb202df7b7d1c8ffaa8ed35
Mihail <41691809+Koallla@users.noreply.github.com>
2021-01-02 16:28:07 +0200

    добавил рекурсию

--- lesson4/hw1.py
+++ lesson4/hw1.py
@@ -1,82 +1,104 @@
 import os
 import sys
+from pathlib import Path
 
 # path содержит первый аргумент, считаем, что это валидный адрес в файловой системе
-path = sys.argv[1]
-print(path)
+# path = sys.argv[1]
+# print(path)
 
 # files - это список имен файлов и папок в path.
-files = os.listdir(path)
+# files = os.listdir(path)
 
-photo = []
-video = []
-docs = []
-music = []
-soft = []
-zip_data = []
-other = []
-directs = []
-unique_extension = set()
+path_dir = 'D:\\Install'
+path = Path(path_dir)
 
-# Выделение уникальных расширений
-def extension(file, container):
-    if '.' in file:
-        container.add(file.split('.')[-1])
+files = list()
+
+# print(path.parent)
 
 
-def sort_files(some_files):
+def get_files_list(path):
 
-    for file in some_files:
+    for file in path.iterdir():
 
-        file_lower = file.lower()
+        if file.is_file():
 
-        if file_lower.endswith('jpg') | file_lower.endswith('jpeg') | file_lower.endswith('png'):
-            photo.append(file)
-        elif file_lower.endswith('avi') | file_lower.endswith('mp4') | file_lower.endswith('mov'):
-            video.append(file)
-        elif file_lower.endswith('doc') | file_lower.endswith('docx') | file_lower.endswith('txt') | file_lower.endswith('pdf') | file_lower.endswith('xlsx'):
-            docs.append(file)
-        elif file_lower.endswith('mp3') | file_lower.endswith('ogg') | file_lower.endswith('wav') | file_lower.endswith('amr'):
-            music.append(file)
-        elif file_lower.endswith('exe'):
-            soft.append(file)
-        elif file_lower.endswith('zip') | file_lower.endswith('rar'):
-            zip_data.append(file)
+            files.append(file.name)
 
-            # =====================================================
-        elif '.' not in file_lower:
-            directs.append(file)
-            # print(path(directs))
-            
         else:
-            other.append(file)
+
+            path = Path(f'{file.parent}\{file.name}')
+            get_files_list(path)
 
 
-        # Только уникальные расширения
-        extension(file_lower, unique_extension)
+get_files_list(path)
+
+print(len(files))
+
+
+
+
+# print(files)
+# photo = []
+# video = []
+# docs = []
+# music = []
+# soft = []
+# zip_data = []
+# other = []
+# directs = []
+# unique_extension = set()
+
+# # Выделение уникальных расширений
+# def extension(file, container):
+#     if '.' in file:
+#         container.add(file.split('.')[-1])
+
+
+# def sort_files(some_files):
+
+#     for file in some_files:
+
+#         file_lower = file.lower()
+
+#         if file_lower.endswith('jpg') | file_lower.endswith('jpeg') | file_lower.endswith('png'):
+#             photo.append(file)
+#         elif file_lower.endswith('avi') | file_lower.endswith('mp4') | file_lower.endswith('mov'):
+#             video.append(file)
+#         elif file_lower.endswith('doc') | file_lower.endswith('docx') | file_lower.endswith('txt') | file_lower.endswith('pdf') | file_lower.endswith('xlsx'):
+#             docs.append(file)
+#         elif file_lower.endswith('mp3') | file_lower.endswith('ogg') | file_lower.endswith('wav') | file_lower.endswith('amr'):
+#             music.append(file)
+#         elif file_lower.endswith('exe'):
+#             soft.append(file)
+#         elif file_lower.endswith('zip') | file_lower.endswith('rar'):
+#             zip_data.append(file)
+
+#             # =====================================================
+#         elif '.' not in file_lower:
+#             directs.append(file)
+#             # print(path(directs))
+            
+#         else:
+#             other.append(file)
+
+
+#         # Только уникальные расширения
+#         extension(file_lower, unique_extension)
 
     
 
-    print('photo', len(photo), photo)
-    print('video', len(video), video)
-    print('docs', len(docs), docs)
-    print('music', len(music), music)
-    print('soft', len(soft), soft)
-    print('zip_data', len(zip_data), zip_data)
-    print('other', len(other), other)
-    print('directs', len(directs), directs)
-    print('unique_extension:', len(unique_extension), unique_extension)
+#     print('photo', len(photo), photo)
+#     print('video', len(video), video)
+#     print('docs', len(docs), docs)
+#     print('music', len(music), music)
+#     print('soft', len(soft), soft)
+#     print('zip_data', len(zip_data), zip_data)
+#     print('other', len(other), other)
+#     print('directs', len(directs), directs)
+#     print('unique_extension:', len(unique_extension), unique_extension)
 
 
 
-sort_files(files)
+# sort_files(files)
 
-# def factorial(n):
-#     if n == 0:
-#         return 1
-#     else:
-#         return n * factorial(n - 1)
-
-# print(factorial(5))
-
-# 5469 5000 1664 1682
