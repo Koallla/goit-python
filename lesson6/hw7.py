ce6ef843fa15a8e0aad8142e2172747858c4203d
Mihail <41691809+Koallla@users.noreply.github.com>
2021-01-31 20:33:36 +0200

    added unpack_archives

--- lesson6/hw7.py
+++ lesson6/hw7.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 from pathlib import Path
 from translator import normalize
 
@@ -7,14 +8,13 @@ from translator import normalize
 path_dir = 'D:\\Auto'
 path = Path(path_dir)
 
+
 photo = []
 video = []
 docs = []
 music = []
-soft = []
 zip_data = []
-unknown_extensions = set()
-unique_extensions = set()
+# unknown_files = []
 
 # Фильтры для файлов
 photo_filter = ("jpeg", "png", "jpg", "svg")
@@ -22,7 +22,16 @@ video_filter = ("avi", "mp4", "mov", "mkv")
 docs_filter = ("doc", "docx", "txt", "pdf", "xlsx", "pptx")
 music_filter = ("mp3", "ogg", "wav", "amr")
 soft_filter = ("exe", "mdf", "mds")
-zip_data_filter = ("zip", "gz", "rar")
+zip_data_filter = ("zip", "bztar", "gztar", "tar", "xztar")
+
+
+
+        
+    
+
+
+
+ignore_dir = ('images', 'video', 'audio', 'documents', 'archives', 'unknown_files')
 
 # Выделение уникальных расширений
 def extensions (file, container):
@@ -31,11 +40,7 @@ def extensions (file, container):
 # Перевод имени файлов и их переименование (для цикла path.iterdir())
 def rename_files (file):
     ext = file.suffix
-    print(file.name)
-
     file_name_without_ext = file.name.removesuffix(ext)
-    print('file_name_without_ext:', file_name_without_ext)
-
     file_name_translated = normalize(file_name_without_ext)
     file_name_with_ext = '{}{}'.format(file_name_translated, ext)
     p = Path(file)
@@ -43,74 +48,118 @@ def rename_files (file):
     full_path_new_file = '{}\{}'.format(parent_dir, file_name_with_ext)
     p.rename(full_path_new_file)
 
+# Создание папки и перемещение в нее файлов определенного типа
+def remove_files (name_new_dir, file):
+
+    # Путь для новой директории
+    path_new_dir = '{}\\{}'.format(path_dir, name_new_dir)
+    p = Path(path_new_dir)
+
+    # Создаем новую директорию и перемещаем в нее файл
+    try:
+        Path(path_new_dir).mkdir()
+        shutil.move(file, path_new_dir)
+    except FileExistsError:
+        shutil.move(file, path_new_dir)
+       
 
    
 # Получение всех файлов, в том числе вложенных, 
-all_files = list()  
-
 def get_files_list(path):
-    for file in path.iterdir():
+    
 
+    # Перевод всех файлов на латиницу
+    for file in path.iterdir():
+        rename_files(file)
+
+    for file in path.iterdir():
         if file.is_file():
-            rename_files(file)
-        
-            all_files.append(file.name)
-            # Получение уникальных расширений
-            extensions(file, unique_extensions)
+
+            # all_files.append(file.name)
+            # # Получение уникальных расширений
+            # extensions(file, unique_extensions_tuple)
 
             ext_lower = file.suffix[1:].lower()
-            file_name = file.name
 
-            # if ext_lower in (photo_filter):
-            #     photo.append(file_name)
+            if ext_lower in (photo_filter):
+                photo.append(file.name)
+                remove_files('images', file)
+                
+            elif ext_lower in (video_filter):
+                video.append(file.name)
+                remove_files('video', file)
 
-            # elif ext_lower in (video_filter):
-            #     video.append(file_name)
+            elif ext_lower in (docs_filter):
+                docs.append(file.name)
+                remove_files('documents', file)
 
-            # elif ext_lower in (docs_filter):
-            #     docs.append(file_name)
+            elif ext_lower in (music_filter):
+                music.append(file.name)
+                remove_files('audio', file)
+            
+            elif ext_lower in (music_filter):
+                music.append(file.name)
+                remove_files('audio', file)
+            
+            elif ext_lower in (zip_data_filter):
+                zip_data.append(file.name)
+                path_for_dir_archives = '{}\\{}'.format(path_dir, 'archives')
+                if os.path.exists(path_for_dir_archives):
+                    path_for_dir_unpack = '{}\\{}'.format(path_for_dir_archives, file.name.removesuffix(file.suffix[:]))
+                    os.mkdir(path_for_dir_unpack)
+                    shutil.unpack_archive(file, path_for_dir_unpack)
+                else:
+                    os.mkdir(path_for_dir_archives)
+                    path_for_dir_unpack = '{}\\{}'.format(path_for_dir_archives, file.name.removesuffix(file.suffix[:]))
+                    os.mkdir(path_for_dir_unpack)
+                    shutil.unpack_archive(file, path_for_dir_unpack)
 
-            # elif ext_lower in (music_filter):
-            #     music.append(file_name)
 
-            # elif ext_lower in (soft_filter):
-            #     soft.append(file_name)
+            # else:
+            #     unknown_files.append(file.name)
+            #     remove_files('unknown_files', file)
 
-            # elif ext_lower in (zip_data_filter):
-            #     zip_data.append(file_name)
+                
+        elif file.is_dir() and file.name in ignore_dir:
+            continue
 
-        #     else:
-        #         extensions(file, unknown_extensions)
+        else:
+            # Создаем новый путь
+            path_for_recursion = Path(f'{file.parent}\{file.name}')
+            # Рекурсия
+            get_files_list(path_for_recursion)
+            # Удаление пустых директорий
+            try:
+                Path.rmdir(file)
+            except OSError:
+                continue
 
-        # else:
-        #     path = Path(f'{file.parent}\{file.name}')
-        #     # Рекурсия
-        #     get_files_list(path)
-    
+
 get_files_list(path)
 
 
 
-# def create_table_string_format(extention, files_list):
-#     ''' This funcrion create table for files_list '''
+def create_table_string_format(extention, files_list):
+    ''' This funcrion create table for files_list '''
 
-#         width = 20
-#         width_file_list = 60
+    width = 20
+    width_file_list = 60
 
-#         string_files = ''
-#         for file in files_list:
-#             string_files += '| {:^{width}} | {:^{width}} | {:^{width_file_list}} | \n'.format(' ', ' ', file, width=width, width_file_list=width_file_list)
+    string_files = ''
+    for file in files_list:
+        string_files += '| {:^{width}} | {:^{width}} | {:^{width_file_list}} | \n'.format(' ', ' ', file, width=width, width_file_list=width_file_list)
 
 
-#         title = '| {:^{width}} | {:^{width}} | {:^{width_file_list}} |'.format('Files names', 'Counts', 'Files list',  width=width, width_file_list=width_file_list)
-#         line = '=' * len(title)
-#         header = line + '\n' + title + '\n' + line + '\n'
+    title = '| {:^{width}} | {:^{width}} | {:^{width_file_list}} |'.format('Files names', 'Counts', 'Files list',  width=width, width_file_list=width_file_list)
+    line = '=' * len(title)
+    header = line + '\n' + title + '\n' + line + '\n'
 
-#         files_name_and_count = '| {:^{width}} | {:^{width}} | {:_^{width_file_list}} | \n'.format(extention, len(files_list), '', width=width, width_file_list=width_file_list)
+    files_name_and_count = '| {:^{width}} | {:^{width}} | {:_^{width_file_list}} | \n'.format(extention, len(files_list), '', width=width, width_file_list=width_file_list)
 
-#         end = '{:=^{width}}'.format('END', width=len(title))
+    end = '{:=^{width}}'.format('END', width=len(title))
 
-#         print(header + files_name_and_count + string_files + end)
+    print(header + files_name_and_count + string_files + end)
+
 
 
 
@@ -118,9 +167,10 @@ get_files_list(path)
 # create_table_string_format('video', video)
 # create_table_string_format('docs', docs)
 # create_table_string_format('music', music)
-# create_table_string_format('soft', soft)
+
+
 # create_table_string_format('zip_data', zip_data)
-# create_table_string_format('unknown_extensions', unknown_extensions)
-# create_table_string_format('all_extension:', unique_extensions)
+# create_table_string_format('unknown_files', unknown_files)
+
 
 
