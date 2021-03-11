29b8fa62f4b78fe299269b62575e26bab966e1f5
Mihail <41691809+Koallla@users.noreply.github.com>
2021-03-11 19:40:28 +0200

    fixed added method

--- lesson10/hw10.py
+++ lesson10/hw10.py
@@ -3,38 +3,47 @@ from collections import UserDict
 
 class AddressBook(UserDict):
 
-    def add_record(self, record):
-        self.data[record.name.value] = record.phone
+    def add_record(self, name, *phone):
+        self.data.update(Record(name, phone).data_dict)
 
     def show_all(self):
         print(self.data)
 
 
 class Record:
-    phone = []
+    data_dict = {}
 
     def __init__(self, name, phone):
+        self.key = Name(name).name
+        # self.name = Name(name).
+        self.phone = Phone(list(phone))
 
-        self.name = name
-        self.phone.append(phone.value)
+        if self.key not in self.data_dict:
+            self.data_dict[self.key] = self.phone.phone
 
-    def add_number(self, number):
-        self.phone.append(number)
 
-    def delete_number(self, number):
-        self.phone.remove(number)
+    # def add_number(self, number):
+    #     self.phone.append(number)
+    #
+    # def delete_number(self, number):
+    #     self.phone.remove(number)
 
 
 class Field:
-    def __init__(self, value):
-        self.value = value
+    pass
 
 
 class Name(Field):
-    pass
+    def __init__(self, name):
+        self.name = name
+
 
 
 class Phone(Field):
-    pass
+    def __init__(self, phone):
+        self.phone = phone
 
 
+a = AddressBook()
+a.add_record('Kirill', 1111111, 2222222, 55555555)
+a.show_all()
