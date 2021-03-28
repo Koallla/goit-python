3d0f62b6d1c138f4ed9db375c4d2493157742e9c
Mihail <41691809+Koallla@users.noreply.github.com>
2021-03-28 19:51:10 +0300

    fixed phone and birthday

--- lesson11/HW11.py
+++ lesson11/HW11.py
@@ -31,26 +31,22 @@ class AddressBook(UserDict):
 
 
 class Record:
-
     def __init__(self, name, value, *phone, birthday=None):
         self.value = value
         self.name = name
         if phone:
-            p = Phone()
-            p.value = phone
-            self.phones = p.value
+            # p = Phone()
+            # p.value = phone
+            self.phones = Phone(phone).value
         if birthday:
-            b = Birthday()
-            b.value = birthday
-            self.birthday = b.value
+            self.birthday = Birthday(birthday).value
 
     def add_data(self, field, *data):
         if field == 'birthday':
-            b = Birthday()
-            b.value = str(*data)
+            b = Birthday(str(*data))
             self.birthday = b.value
         else:
-            p = Phone()
+            p = Phone(data)
             p.value = data
             if len(p.value) == 1:
                 self.phones.append(p.value[0])
@@ -75,7 +71,10 @@ class Record:
             print(f'{dif.days} days')
 
     def __str__(self):
-        return f'{self.value} {self.phones} {self.birthday}'
+        try:
+            return f'{self.value}: {self.phones}, birthday: {self.birthday.date()}'
+        except AttributeError:
+            return f'{self.value} {self.phones}'
 
 
 class Field:
@@ -88,14 +87,29 @@ class Name(Field):
 
 
 class Phone(Field):
-    def __init__(self):
+    def __init__(self, value):
         self.__value = []
+        self.value = value
 
     def __getitem__(self, key=None):
         return self.__value
 
     def __setitem__(self, key, value):
-        self.value = value
+        if type(value) != int:
+            raise Exception('Only one number and integer')
+        try:
+            check_phone_number(value)
+            if len(self.__value) and key < (len(self.__value)):
+                self.__value.pop(key)
+                self.__value.insert(key, value)
+            elif key > (len(self.__value)):
+                raise IndexError
+            else:
+                self.__value.append(value)
+        except WrongPhoneNumberFormat:
+            print('Please, enter number in format 380_________')
+        except IndexError:
+            print(f'Max index {len(self.__value)}')
 
     @property
     def value(self):
@@ -114,11 +128,15 @@ class Phone(Field):
         except WrongPhoneNumberFormat:
             print('Please, enter number in format 380_________')
 
+    def __str__(self):
+        return f'Phone: {self.__value}'
+
 
 class Birthday(Field):
 
-    def __init__(self):
+    def __init__(self, value):
         self.__value = None
+        self.value = value
 
     def __getitem__(self, key=None):
         return self.__value
@@ -138,35 +156,57 @@ class Birthday(Field):
         except WrongDateFormat:
             print('Please, input birthday date in format "%d %m %Y" ')
 
-    def __str__(self):
-        return f'Birthday: {self.__value}'
+    # def __str__(self):
+    #     return f'Birthday: {self.__value}'
 
 
 
 
-
-
-#
-# user = Record('user', 'phone', 380953128882, 380506042357, birthday='11 07 1983')
+user = Record('user', 'phone', 380953128882, 380506042357)
 # print(user)
-# user1 = Record('user1', 'phone', 380953128882, 380506042357, birthday='23 07 2014')
-# user2 = Record('user2', 'phone', 380953128882, 380506042357, birthday='12 01 1983')
-# user3 = Record('user3', 'phone', 380953128882, 380506042357, birthday='12 01 1983')
+user1 = Record('user1', 'phone', 380953128882, 380506042357, birthday='23 07 2014')
+user2 = Record('user2', 'phone', 380953128882, 380506042357, birthday='12 01 1983')
+user3 = Record('user3', 'phone', 380953128882, 380506042357, birthday='12 01 1983')
 # user.add_data('phone', 380991245367, 380995555555)
 # print(user)
-# user.add_data('birthday', '12 01 1983')
-# print(user)
-# user.delete_data_in_field('birthday')
-# print(user)
-# user.add_data('birthday', '14 04 2011')
-# print(user)
-# user.days_to_birthday()
+# user2.add_data('birthday', '12 01 1983')
+# print(user2)
+# user2.delete_data_in_field('birthday')
+# print(user2)
+# user2.add_data('birthday', '14 04 2011')
+# print(user2)
+# user2.days_to_birthday()
 #
-# a = AddressBook()
-# a.add_record(user)
-# a.add_record(user1)
-# a.add_record(user2)
-# a.add_record(user3)
-#
-# a.iterator(3)
+# user2.delete_data_in_field('phone')
+# print(user2)
+
+
+
+a = AddressBook()
+a.add_record(user)
+a.add_record(user1)
+a.add_record(user2)
+a.add_record(user3)
+
+a.iterator(2)
 # print(a)
+
+# p = Phone(380953128882)
+# print(p)
+# p[0] = 380953128883
+# print(p)
+# p[1] = 380953128884
+# print(p)
+# p[1] = 380953128885
+# print(p)
+# p[2] = 380953128888
+# print(p)
+# p[0] = 380953128881
+# print(p)
+#
+
+
+
+
+
+
